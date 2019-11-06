f3247ab2eb0794a8e5f1b2f8f5cd180b0bb25e4a
moonpeter <m88nkhv@naver.com>
2019-11-06 15:47:17 +0900

    feat : with list comprehension

--- python/191106-fizzbuzz.py
+++ python/191106-fizzbuzz.py
@@ -1,6 +1,1 @@
-for i in range(1, 100+1):
-    if i % 3 == 0 or i % 5 == 0:
-        print("fizz" * (i % 3 == 0) + "buzz"*(i % 5 == 0))
-    elss:
-        print(i)
-        
+print(["fizzbuzz" if i%15==0 else "fizz" if i%3==0 else "buzz" if i%5==0 else i for i in range(1,100+1)])
